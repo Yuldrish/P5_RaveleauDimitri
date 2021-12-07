9a9a30e0becd40c4032867637871502b9f1b7765
Dimitri Raveleau <yuldrish@gmail.com>
2021-12-07 18:16:18 +0100

    creation de la recuperation des api et creation de le section et des article

--- front/js/index.js
+++ front/js/index.js
@@ -19,3 +19,46 @@
 **      });
 **    });
 */
+creationSection();
+//Récuperation des produits
+
+async function getArticles(){
+  let appelproduits = await fetch("http://localhost:3000/api/products")
+  return await appelproduits.json();
+}
+
+/*
+**Récuperation des informations des produits
+**Et création des élément de la page
+*/
+async function creationSection(){
+  let resultat = await getArticles()
+    .then(function (reponseAPI) {
+      const produits = reponseAPI;
+      console.table(produits);
+      for (let produit in produits) {
+
+        let lienProduit = document.createElement("a");
+        document.querySelector(".items").appendChild(lienProduit);
+        lienProduit.href = `product.html?id=${reponseAPI[produit]._id}`;
+
+        let articleProduit = document.createElement("article");
+        lienProduit.appendChild(articleProduit);
+
+        let imgProduit = document.createElement("img");
+        articleProduit.appendChild(imgProduit);
+        imgProduit.src = reponseAPI[produit].imageUrl;
+        imgProduit.alt = reponseAPI[produit].altTxt;
+
+        let nomProduit = document.createElement("h3");
+        articleProduit.appendChild(nomProduit);
+        nomProduit.classList.add("productName");
+        nomProduit.innerHTML = reponseAPI[produit].name;
+
+        let descriptionProduit = document.createElement("p");
+        articleProduit.appendChild(descriptionProduit);
+        descriptionProduit.classList.add("productDescription");
+        descriptionProduit.innerHTML = reponseAPI[produit].description;        
+      }
+    })
+}
