2626e5b672d58e69131c9fae4a69cdf15136301f
Dimitri Raveleau <yuldrish@gmail.com>
2021-12-08 17:00:36 +0100

    ajout de commentaire pour deffinir les élement du code

--- front/js/product.js
+++ front/js/product.js
@@ -17,6 +17,7 @@ function getArticle() {
             return resultat.json();
         })
 
+        //Répartition des données dans le DOM
         .then(async function(reponseAPI) {
             produit = await reponseAPI;
             console.table(produit);
@@ -29,20 +30,42 @@ function getArticle() {
 //Création des éléments de la page
 function getPost(produit) {
 
+    /*
+    **Création de l'élément "img" 
+    **Import de ça source et sont alt
+    **Dans l'élémént possédant la class "item__img"
+    */
     let imgProduit = document.createElement("img");
     document.querySelector(".item__img").appendChild(imgProduit);
     imgProduit.src = produit.imageUrl;
     imgProduit.alt = produit.altTxt;
 
+    /*
+    **Import du nom du produit
+    **Dans l'élément possédant l'id "title"
+    */
     let nomProduit = document.getElementById("title");
     nomProduit.innerHTML = produit.name;
 
+    /*
+    **Import du prix du produit
+    **Dans l'élément possédant l'id "price"
+    */
     let prixProduit = document.getElementById("price");
     prixProduit.innerHTML = produit.price;
 
+    /*
+    **Import de la description du produit
+    **Dans lélément possédant l'id "description"
+    */
     let descriptionProduit = document.getElementById("description");
     descriptionProduit.innerHTML = produit.description;
 
+    /*
+    **Création de l'élément "option"
+    **Import de ces source permetant le choix de la couleur
+    **Dans l'élément possédant l'id "color"
+    */
     for (let couleurs of produit.colors){
         console.table(couleurs);
         let couleurProduit = document.createElement("option");
@@ -57,12 +80,19 @@ function getPost(produit) {
 function addToCart(produit) {
     const btn_EnvoiAuPanier = document.querySelector("#addToCart");
 
+    /*
+    **Ecouter le choix avec deux condition:
+    **--Choix de la couleur non nulle
+    **--Choix de la quantiter entre 1 et 100
+    */
     btn_EnvoiAuPanier.addEventListener("click", (event) => {
         if (quantiterChoisie.value > 0 && quantiterChoisie.value <= 100 && quantiterChoisie.value != 0 ){
 
+            //Import du choix de la couleur et de la quantiter
             let choixCouleur = couleurChoisie.value;
             let choixQuantiter = quantiterChoisie.value;
 
+            //Import des options du produit choisi
             let optionsProduit = {
                 idProduit: idProduit,
                 couleurProduit: choixCouleur,
@@ -74,23 +104,29 @@ function addToCart(produit) {
                 imgAltProduit: produit.altTxt,
             };
 
+            //Initialisation du stockage local
             let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
+            //import dans le panier
+            //Si le panier à déjà un produit
             if (produitLocalStorage) {
                 const resultatPanier = produitLocalStorage.find(
                     (element) => element.idProduit === idProduit && element.couleurProduit === choixCouleur);
                     
+                    //Si le produit commander est identique
                     if (resultatPanier) {
                         let nouvelleQuantiter =
                         parseInt(optionsProduit.quantiterProduit) + parseInt(resultatPanier.quantiterProduit);
                         resultatPanier.quantiterProduit = nouvelleQuantiter;
                         localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
                         console.table(produitLocalStorage);
-                    } else {
+                     //Si le produit commander est différent
+                    } else  {
                         produitLocalStorage.push(optionsProduit);
                         localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
                         console.table(produitLocalStorage);
                     }
+             //Si le panier est vide
             } else {
                 produitLocalStorage =[];
                 produitLocalStorage.push(optionsProduit);
