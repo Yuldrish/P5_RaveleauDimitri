b12c6af4aae65a6031a1b2f5e2417e39ac94f93c
Dimitri Raveleau <yuldrish@gmail.com>
2021-12-08 11:51:52 +0100

    ajouts des commentaires sur les variables et d'un ".catch" en cas d'erreur

--- front/js/index.js
+++ front/js/index.js
@@ -37,28 +37,54 @@ async function creationSection(){
       const produits = reponseAPI;
       console.table(produits);
       for (let produit in produits) {
-
+        
+        /*
+        **Création de l'élément "a"
+        **dans l'élément possédant la classe "items"
+        */
         let lienProduit = document.createElement("a");
         document.querySelector(".items").appendChild(lienProduit);
         lienProduit.href = `product.html?id=${reponseAPI[produit]._id}`;
 
+        /*
+        **Création de l'élément "article" 
+        **dans l'élément "a"
+        */
         let articleProduit = document.createElement("article");
         lienProduit.appendChild(articleProduit);
 
+        /*
+        **Création de l'élément "img" 
+        **import de ça source et sont alt
+        **Dans l'élémént "article"
+        */
         let imgProduit = document.createElement("img");
         articleProduit.appendChild(imgProduit);
         imgProduit.src = reponseAPI[produit].imageUrl;
         imgProduit.alt = reponseAPI[produit].altTxt;
 
+        /*
+        **Création de l'élément "h3" 
+        **import de son nom et ajout de sa classe "productName"
+        **Dans l'élément "article"
+        */
         let nomProduit = document.createElement("h3");
         articleProduit.appendChild(nomProduit);
         nomProduit.classList.add("productName");
         nomProduit.innerHTML = reponseAPI[produit].name;
-
+        
+        /*
+        **Création de l'élément "p"
+        **import de la description et ajout de sa classe "productDescription"
+        **Dans l'élément "article"
+        */ 
         let descriptionProduit = document.createElement("p");
         articleProduit.appendChild(descriptionProduit);
         descriptionProduit.classList.add("productDescription");
         descriptionProduit.innerHTML = reponseAPI[produit].description;        
       }
+    })
+    .catch(function(erreur) {
+      return erreur
     })
 }
