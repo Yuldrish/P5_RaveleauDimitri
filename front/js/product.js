73ba4ccaac0eb4d9880209da8a6b6cb6e9e9a189
Dimitri Raveleau <yuldrish@gmail.com>
2021-12-15 16:37:12 +0100

    version test avec les modification apporter pas le mentore, plus debut de rectification des erreur de fonctionnement

--- front/js/product.js
+++ front/js/product.js
@@ -1,28 +1,29 @@
-let str = window.location.href;
-let url = new URL(str);
-let idProduit = url.searchParams.get("id");
-console.log(idProduit);
-let produit = "";
-
-const couleurChoisie = document.querySelector("#colors");
-const quantiterChoisie = document.querySelector("#quantity");
-
-getArticle();
+// Une fois que le dom est chargé on execute comme suit
+document.addEventListener("DOMContentLoaded", async (event) => {
+  // Recuperation de l'id du produit
+  const idProduit = new URL(window.location.href).searchParams.get("id");
+  console.log(idProduit);
+  // Recuperation du dit produit via l'API
+  const produit = await getArticle(idProduit);
+  // Mise en place des elements html sur la page
+  getPost(produit);
+});
 
 //Récuperation des produits API
-async function getArticle() {
+async function getArticle(idProduit) {
   const resultat = await fetch(
     `http://localhost:3000/api/products/${idProduit}`
   );
-  const produit = await resultat.json()
-  //Répartition des données dans le DOM
+  const produit = await resultat.json();
   console.table(produit);
-  if (produit) {
-    getPost(produit);
-  }
+  // Le role principale de cette fonction devrait etre la recuperation du produit du backend.
+  // De ce fait il ne dois plus se charger d'appeler la fonction getPost() qui lui se charge de la creation des elements de la page.
+  return produit;
 }
 
-//Création des éléments de la page
+// Création des éléments de la page.
+// Je pense qu'il faut changer le nom de cette fonction. Il n'est pas tres explicite
+// Un nom approprié pourrait etre "initProductPage()"
 function getPost(produit) {
   /*
    **Création de l'élément "img"
@@ -67,28 +68,27 @@ function getPost(produit) {
     couleurProduit.value = couleurs;
     couleurProduit.innerHTML = couleurs;
   }
-  // addToCart(produit)
-  const btn_envoiAuPanier = document.querySelector("#addToCart");
-  btn_envoiAuPanier.addEventListener("click", () => addToCart(produit));
+
+  // Ici, au lieu d'appeler la fonction "addToCard()", on la greffe juste a l'evenement click du bouton de commande
+  document.querySelector("#addToCart").addEventListener("click", (event) => {
+    addToCart(produit);
+  });
 }
 
-//Gestion du choix du produit
+// Gestion du choix du produit
+// Devra etre lancé uniquement au click du bouton commander
 function addToCart(produit) {
+  // Import du choix de la couleur et de la quantiter
+  const choixCouleur = document.querySelector("#colors").value;
+  const choixQuantiter = document.querySelector("#quantity").value;
+
   /*
    **Ecouter le choix avec deux condition:
    **--Choix de la couleur non nulle
    **--Choix de la quantiter entre 1 et 100
    */
-  if (
-    quantiterChoisie.value > 0 &&
-    quantiterChoisie.value <= 100 &&
-    couleurChoisie.value != ""
-  ) {
-    //Import du choix de la couleur et de la quantiter
-    let choixCouleur = couleurChoisie.value;
-    let choixQuantiter = quantiterChoisie.value;
-
-    //Import des options du produit choisi
+  if (choixQuantiter > 0 && choixQuantiter <= 100 && choixCouleur != "") {
+    // Import des options du produit choisi
     let optionsProduit = {
       idProduit: idProduit,
       couleurProduit: choixCouleur,
@@ -100,29 +100,28 @@ function addToCart(produit) {
       imgAltProduit: produit.altTxt,
     };
 
-    //Initialisation du stockage local
+    // Initialisation du stockage local
     let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
-    ////////// Test popup //////////
+    // Crétion d'un popup pour signaler l'ajout du produit au panier
     const confirmationPopup = () => {
       if (
         window.confirm(`Votre comande de ${choixQuantiter} ${produit.name} ${choixCouleur} à été ajouter au panier, 
-                Cliquer sur "OK" pour allez au panier.`)
+                      Cliquer sur "OK" pour allez au panier.`)
       ) {
         window.location.replace("http://127.0.0.1:5500/front/html/cart.html");
       }
     };
 
-    //import dans le stockage local
-    //Si le panier à déjà un produit
+    // import dans le stockage local
+    // Si le panier à déjà un produit
     if (produitLocalStorage) {
       const resultatPanier = produitLocalStorage.find(
         (element) =>
           element.idProduit === idProduit &&
           element.couleurProduit === choixCouleur
       );
-
-      //Si le produit commander est identique
+      // Si le produit commander est identique
       if (resultatPanier) {
         let nouvelleQuantiter =
           parseInt(optionsProduit.quantiterProduit) +
@@ -131,14 +130,14 @@ function addToCart(produit) {
         localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
         console.table(produitLocalStorage);
         confirmationPopup();
-        //Si le produit commander est différent
+        // Si le produit commander est différent
       } else {
         produitLocalStorage.push(optionsProduit);
         localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
         console.table(produitLocalStorage);
         confirmationPopup();
       }
-      //Si le panier est vide
+      // Si le panier est vide
     } else {
       produitLocalStorage = [];
       produitLocalStorage.push(optionsProduit);
